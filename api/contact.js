a2499ef0ff0dd86cdf42af2cf908ca363a8dbfd8
Djuc1 <victoregbuedo@gmail.com>
2026-08-21 03:57:02 +0100

    correcting email

--- api/contact.js
+++ api/contact.js
@@ -21,7 +21,7 @@ module.exports = async (req, res) => {
 
   try {
     await transporter.sendMail({
-      from: `"${name}" <${process.env.EMAIL_USER}>`,
+      from: `"${name}"`,
       to: process.env.EMAIL_USER,
       replyTo: email,
       subject: `New Portfolio Message from ${name}`,
